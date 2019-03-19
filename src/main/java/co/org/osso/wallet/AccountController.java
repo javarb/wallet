c3d09ae669791e0712a22d7ebe005d594828462a
Javier Arboleda <reivaj49@gmail.com>
2019-03-19 09:32:11 -0500

    Add AccountController

---
+++ src/main/java/co/org/osso/wallet/AccountController.java
@@ -0,0 +1,22 @@
+package co.org.osso.wallet;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wallet")
+public class AccountController {
+
+    private final accountService AccountService;
+
+    public AccountController(AccountService accountService){
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/accounts")
+    public ListAccountsResponse listAccounts(){
+        return accountService.listAccounts();
+    }
+
+}

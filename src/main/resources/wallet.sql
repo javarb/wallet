554aba31723e250eef704a41fe2485327b7dccef
Javier Arboleda <reivaj49@gmail.com>
2019-04-02 11:30:12 -0500

    Adding index to 'id' column

--- src/main/resources/wallet.sql
+++ src/main/resources/wallet.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS account (
 	id INT NOT NULL AUTO_INCREMENT COMMENT 'unique ID for each account',
 	account_name varchar(20) NOT NULL COMMENT 'the account name' ,
 	fullname varchar(255) NOT NULL COMMENT 'fullname of the account owner',
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	INDEX (id)
 );
 
 -- Some test data

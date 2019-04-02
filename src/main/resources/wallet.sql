95f2c355aeef065ff775c8f14573e334085bf000
Javier Arboleda <reivaj49@gmail.com>
2019-04-02 08:08:50 -0500

    Add SQL DDL

---
+++ src/main/resources/wallet.sql
@@ -0,0 +1,14 @@
+-- Table structure for table 'account'
+-- Use the default Engine INNODB 
+CREATE TABLE IF NOT EXISTS account (
+	id INT NOT NULL AUTO_INCREMENT COMMENT 'unique ID for each account',
+	account_name varchar(20) NOT NULL COMMENT 'the account name' ,
+	fullname varchar(255) NOT NULL COMMENT 'fullname of the account owner',
+	PRIMARY KEY (id)
+);
+
+-- Some test data
+INSERT INTO account(account_name,fullname)
+VALUES	('user1', 'Juan'),
+	('user2', 'Pedro'),
+ 	('user3', 'Pablo');

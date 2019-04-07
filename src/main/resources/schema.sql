ac3d79fac57912f1399b6278fb018e2814d0ea05
Javier A. Arboleda <reivaj49@gmail.com>
2019-04-07 16:41:32 -0500

    Minor change

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,6 +4,5 @@ CREATE TABLE IF NOT EXISTS account (
 	id INT NOT NULL AUTO_INCREMENT COMMENT 'unique ID for each account',
 	account_name varchar(20) NOT NULL COMMENT 'the account name' ,
 	fullname varchar(255) NOT NULL COMMENT 'fullname of the account owner',
-	PRIMARY KEY (id),
-	INDEX (id)
+	PRIMARY KEY (id)
 );

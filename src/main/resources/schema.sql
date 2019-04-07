3c97d7b5720d4a4316cfde2d8b5f04e7c12d7c6a
Javier A. Arboleda <reivaj49@gmail.com>
2019-04-07 16:44:08 -0500

    Capitalize comments

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 -- Table structure for table 'account'
 -- Use the default Engine INNODB 
 CREATE TABLE IF NOT EXISTS account (
-	id BIGINT NOT NULL AUTO_INCREMENT COMMENT 'unique ID for each account',
-	account_name varchar(20) NOT NULL COMMENT 'the account name' ,
-	fullname varchar(255) NOT NULL COMMENT 'fullname of the account owner',
+	id BIGINT NOT NULL AUTO_INCREMENT COMMENT 'Unique ID for each account',
+	account_name varchar(20) NOT NULL COMMENT 'The account name' ,
+	fullname varchar(255) NOT NULL COMMENT 'Fullname of the account owner',
 	PRIMARY KEY (id)
 );

38d2fe12bc98704688bb2e6264d62107cd195307
Javier A. Arboleda <reivaj49@gmail.com>
2019-04-07 23:09:09 -0500

    Add some unit tests

--- src/test/java/co/org/osso/wallet/WalletApplicationTests.java
+++ src/test/java/co/org/osso/wallet/WalletApplicationTests.java
@@ -1,5 +1,6 @@
 package co.org.osso.wallet;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +10,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class WalletApplicationTests {
 
+    private AccountService AccountServiceTarget = new AccountService();
+
     @Test
-    public void contextLoads() {
+    public void inMemoryAccountsNotNull() {
+        ListAccountsResponse accounts = AccountServiceTarget.listAccounts();
+        Assert.assertNotNull(accounts);
+
     }
 
+    @Test
+    public void inMemoryAccountNameEqualsTo() {
+        ListAccountsResponse accounts = AccountServiceTarget.listAccounts();
+        Assert.assertEquals("user_1", accounts.getData().get(1).getAccount());
+
+    }
 }

9cdd0f1d4c91351455e8c964a9401a8b903ea622
Javier Arboleda <reivaj49@gmail.com>
2019-04-12 12:13:11 -0500

    Change to Long the primary key datatype

--- src/main/java/co/org/osso/wallet/AccountRepository.java
+++ src/main/java/co/org/osso/wallet/AccountRepository.java
@@ -2,5 +2,5 @@ package co.org.osso.wallet;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface AccountRepository extends CrudRepository<Account, Integer> {
+public interface AccountRepository extends CrudRepository<Account, Long> {
 }

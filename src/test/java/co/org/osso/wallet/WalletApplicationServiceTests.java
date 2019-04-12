d78f8adfe847b6a6b7447fa5a2accc602d0b718d
Javier Arboleda <reivaj49@gmail.com>
2019-04-12 16:00:25 -0500

    Rename Service tests

--- src/test/java/co/org/osso/wallet/WalletApplicationServiceTests.java
+++ src/test/java/co/org/osso/wallet/WalletApplicationServiceTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WalletApplicationTests {
+public class WalletApplicationServiceTests {
 
     private AccountService AccountServiceTarget = new AccountService();
 
